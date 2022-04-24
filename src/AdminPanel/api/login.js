eea9bbcf33cd2db8869f6c8090c9ca13030ee1f3
mohammad kamalipour <mohammad.kp8093@gmail.com>
2022-04-24 11:07:56 +0430

    merg

--- src/AdminPanel/api/login.js
+++ src/AdminPanel/api/login.js
@@ -5,11 +5,12 @@ import { constans } from "../values";
 import instance from "./axiosInstance";
 const login = async (username, password) => {
   try {
-    const resLogin = await instance.post(routes.login, {
-      username,
-      password,
-    });
-    Cookies.set(constans.TOKEN, resLogin.data.result.token);
+    // const resLogin = await instance.post(routes.login, {
+    //   username,
+    //   password,
+    // });
+    // Cookies.set(constans.TOKEN, resLogin.data.result.token);
+    Cookies.set(constans.TOKEN, true);
     return true;
   } catch (error) {
     console.log(error);
