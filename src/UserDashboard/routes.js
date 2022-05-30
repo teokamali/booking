20700ce12925be99b207f978f271f57928442a07
mohammad kamalipour <mohammad.kp8093@gmail.com>
2022-05-30 16:58:45 +0430

    add property removed from sidebar

--- src/UserDashboard/routes.js
+++ src/UserDashboard/routes.js
@@ -9,53 +9,53 @@ import AddProperty from "./layouts/AddProperty";
 import Icon from "@mui/material/Icon";
 
 const routes = [
-  {
-    type: "collapse",
-    name: "Dashboard",
-    key: "dashboard",
-    icon: <Icon fontSize="small">dashboard</Icon>,
-    route: "/",
-    link: "/dashboard",
-    component: <Dashboard />,
-  },
-  {
-    type: "collapse",
-    name: "Gallery",
-    key: "gallery",
-    icon: <Icon fontSize="small">photo library</Icon>,
-    route: "gallery",
-    link: "gallery",
-    component: <Gallery />,
-  },
-  {
-    type: "collapse",
-    name: "Properties",
-    key: "property",
-    icon: <Icon fontSize="small">apartment</Icon>,
-    route: "property",
-    link: "property",
-    component: <Property />,
-  },
-  {
-    type: "collapse",
-    name: "Add Property",
-    key: "property/add",
-    icon: <Icon fontSize="small">add</Icon>,
-    route: "property/add",
-    link: "property/add",
-    component: <AddProperty />,
-  },
+	{
+		type: "collapse",
+		name: "Dashboard",
+		key: "dashboard",
+		icon: <Icon fontSize='small'>dashboard</Icon>,
+		route: "/",
+		link: "/dashboard",
+		component: <Dashboard />,
+	},
+	{
+		type: "collapse",
+		name: "Gallery",
+		key: "gallery",
+		icon: <Icon fontSize='small'>photo library</Icon>,
+		route: "gallery",
+		link: "gallery",
+		component: <Gallery />,
+	},
+	{
+		type: "collapse",
+		name: "Properties",
+		key: "property",
+		icon: <Icon fontSize='small'>apartment</Icon>,
+		route: "property",
+		link: "property",
+		component: <Property />,
+	},
+	{
+		type: "",
+		name: "Add Property",
+		key: "property/add",
+		icon: <Icon fontSize='small'>add</Icon>,
+		route: "property/add",
+		link: "property/add",
+		component: <AddProperty />,
+	},
 
-  {
-    type: "collapse",
-    name: "Setting",
-    key: "Setting",
-    icon: <Icon fontSize="small">settings</Icon>,
-    route: "setting",
-    link: "setting",
+	{
+		type: "collapse",
+		name: "Setting",
+		key: "Setting",
+		icon: <Icon fontSize='small'>settings</Icon>,
+		route: "setting",
+		link: "setting",
 
-    component: <Profile />,
-  },
+		component: <Profile />,
+	},
 ];
 
 export default routes;
