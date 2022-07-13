a1e0994d14b9ae75f24249c89eae98697bb3b4cc
Mohammad Kamalipour <Mohammad.kp8093@gmail.com>
2022-07-13 20:47:58 +0430

    new modal created

--- src/Screens/Page404/index.js
+++ src/Screens/Page404/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal2 from "components/Modal2";
+import { Button } from "components";
 
 const Page404 = () => {
-	return <div>404 not found</div>;
+	const [isOpen, setisOpen] = useState(false);
+	return (
+		<div>
+			<button onClick={() => setisOpen(true)}>open Modal</button>
+			<Modal2
+				id='modal1'
+				title='Modal Mamad'
+				onClose={() => setisOpen(false)}
+				isOpen={isOpen}
+			></Modal2>
+		</div>
+	);
 };
 
 export default Page404;
